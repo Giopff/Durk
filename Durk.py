2ea610401955c4be9298054dfa9172a6ce874ade
PabloEscobar1337 <giorgipapa8@gmail.com>
2021-01-30 23:32:04 +0400

    added example

--- Durk.py
+++ Durk.py
@@ -1,4 +1,4 @@
-from parser import parse
+from Parser import parse
 import tkinter as tk
 ParsedFile=parse('thefile.html')
 
@@ -16,16 +16,11 @@ class Launch():
     def Execute(self):
         self.root.mainloop()
 
+if __name__ == '__main__':
+    x=Launch()
+    # x.Label(ParsedFile['html']['body'][0]['text'][0]['_text'])
+    for i in ParsedFile['html']['body']:
+        for z in i['text']:
+            x.Label(z['_text'])
+    x.Execute()
 
-
-
-# a = tk.Label(root, text ="Hello World") 
-# a.pack() 
-  
-# root.mainloop()
-
-x=Launch()
-x.Label(ParsedFile['html']['body'][0]['text'][0]['_text'])
-x.Execute()
-
-# print(ParsedFile['html']['body'][0]['text'][0]['_text'])
