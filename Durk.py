29609e35391622775d5181ba20b9586ff5959d24
PabloEscobar1337 <giorgipapa8@gmail.com>
2021-01-31 21:06:19 -0800

    doesnt read text tag inside of text tag fix it

--- Durk.py
+++ Durk.py
@@ -1,7 +1,8 @@
 from parser_main import parse
 import tkinter as tk
 ParsedFile=parse('thefile.html')
-
+methods=['text']
+root_path=ParsedFile['html']['body']
 class Launch():
     def __init__(self):
         try:
@@ -9,18 +10,44 @@ class Launch():
         except:
             exit("bruh please write html file properly")
         self.root = tk.Tk()
+        
+        
     def Label(self, text):
         self.label=tk.Label(self.root,text=text)
         self.label.pack()
 
+    
+            
+            
+    
+            
+
     def Execute(self):
         self.root.mainloop()
 
+    def Packer(self,Class):
+            for tag in root_path:
+                for key in tag.keys():
+                    if key in methods:
+                        Check(key,Class)
+
+
+def Check(string,Class):
+    if string=="text":
+        for component in root_path:
+            print(component)
+            for x in component[string]:
+                
+                Class.Label(x['_text'])
+        Class.Execute()
 if __name__ == '__main__':
     x=Launch()
     # x.Label(ParsedFile['html']['body'][0]['text'][0]['_text'])
-    for i in ParsedFile['html']['body']:
-        for z in i['text']:
-            x.Label(z['_text'])
-    x.Execute()
+
+    x.Packer(x)
+
+    # for i in ParsedFile['html']['body']:
+    #     for z in i['text']:
+    #         x.Label(z['_text'])
+    # x.Execute()
 
--- parser_main.py
+++ parser_main.py
@@ -14,6 +14,21 @@ def dictify(r,root=True):
             d[x.tag]=[]
         d[x.tag].append(dictify(x,False))
     return d
+# def listify(r,root=True):
+#     if root:
+#         return {r.tag : listify(r, False)}
+#     d=copy(r.attrib)
+#     # if r.text and r.text[0:3]!='\n  ':
+#         # d["_text"]=r.text
+#     for x in r.findall("./*"):
+#         if x.tag not in d:
+#             d[x.tag]=[]
+#         d[x.tag].append(listify(x,False))
+#     return d
+
+# def parse_tags(fileName):
+#     root = ET.parse(fileName).getroot()
+#     return listify(root)
 
 def parse(fileName):
     root = ET.parse(fileName).getroot()
@@ -22,4 +37,5 @@ def parse(fileName):
 if __name__=="__main__":
     
     print(parse('thefile.html'))
+    # print(parse_tags('thefile.html'))
 
