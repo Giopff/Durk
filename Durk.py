889e2267674b70469bcb2f949d153858cd7a6860
PabloEscobar1337 <giorgipapa8@gmail.com>
2021-02-02 09:35:01 -0800

    added checkbox, radio and date types to input tag

--- Durk.py
+++ Durk.py
@@ -1,6 +1,11 @@
+try:
+    import tkcalendar as tkc
+except:
+    pass
 from parser_main import parse
 import tkinter as tk
-from tkinter.ttk import Combobox
+import tkinter.ttk as ttk
+
 
 ParsedFile=parse('thefile.html')
 methods=['text','button','input']
@@ -31,11 +36,32 @@ class Launch():
         self.Input.pack()
 
     def Comboboxx(self,values, current=None):
-        self.Combobox=Combobox(self.root)
+        self.Combobox=ttk.Combobox(self.root)
         self.Combobox['values']=values
         self.Combobox.current(current)
         self.Combobox.pack()
 
+    def Checkbox(self,text):
+        CBvar = tk.IntVar()
+        CB = tk.Checkbutton(self.root, text=text,variable=CBvar, onvalue=1, offvalue=0)
+        CB.pack()
+
+    def Radio(self,text):
+        R= tk.IntVar()
+        RB = tk.Radiobutton(self.root, text=text, variable=R, value=1)
+        RB.pack()
+
+    def Date(self,text="Pick date"):
+        # self.top = tk.Toplevel(self.root)
+
+        # ttk.Label(self.top, text=text).pack() if you want some popup than this is a good option. i am gonna implement this feature soon
+        try:
+            self.cal = tkc.DateEntry() #top, width=12, background='darkblue', foreground='white', borderwidth=2 RANDOM PARAMEteRS LOL
+        except NameError:
+            exit("install the 'tkcalendar' library (pip install tkcalendar)")
+        except ModuleNotFoundError:
+            exit("install the 'tkcalendar' library (pip install tkcalendar)")
+        self.cal.pack()
 
     def Execute(self):
         self.root.mainloop()
@@ -50,7 +76,6 @@ class Launch():
 def Check(string,Class):
     if string=="text":
         for component in root_path:
-            # print(component)
             for x in component[string]:
                 Class.Label(x['_text'])
     elif string=="button":
@@ -61,14 +86,33 @@ def Check(string,Class):
         for component in root_path:
             for x in component[string]:
                 if "type" in x: #type checking
+                    print(x)
                     if x["type"]=="combobox":
                         temp_list=[]
                         for z in x['option']:
-                            print(z)
                                 
                             temp_list.append(z['value'])
-                        Combobo=Class.Comboboxx(temp_list)
-                        break
+                        Class.Comboboxx(temp_list)
+                        continue
+                    if x["type"]=="checkbox":
+
+                        for z in x['option']:
+
+                            Class.Checkbox(z['value'])
+                        continue
+                    if x["type"]=="radio":
+
+                        for z in x['option']:
+
+                            Class.Radio(z['value'])
+                        continue
+                    if x["type"]=="date":
+                        try:
+                            Class.Date("bruh")
+                        except ModuleNotFoundError:
+                            exit("install the 'tkcalendar' library (pip install tkcalendar)")
+                        continue
+                       
                 Class.Input()
 if __name__ == '__main__':
     x=Launch()
