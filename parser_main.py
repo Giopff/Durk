6e24bf58ca7bf89db330acd56b2b7a5fb8d353d3
PabloEscobar1337 <giorgipapa8@gmail.com>
2021-02-03 08:59:37 -0800

    idk

--- parser_main.py
+++ parser_main.py
@@ -4,31 +4,22 @@ from copy import copy
 
 
 def dictify(r,root=True):
+
     if root:
         return {r.tag : dictify(r, False)}
     d=copy(r.attrib)
+    # print(d)
+    # d['times']=times
     if r.text and r.text[0:3]!='\n  ':
         d["_text"]=r.text
+        
     for x in r.findall("./*"):
+        # print(x.tag)
+        # print(r.findall("./*"))
         if x.tag not in d:
             d[x.tag]=[]
         d[x.tag].append(dictify(x,False))
     return d
-# def listify(r,root=True):
-#     if root:
-#         return {r.tag : listify(r, False)}
-#     d=copy(r.attrib)
-#     # if r.text and r.text[0:3]!='\n  ':
-#         # d["_text"]=r.text
-#     for x in r.findall("./*"):
-#         if x.tag not in d:
-#             d[x.tag]=[]
-#         d[x.tag].append(listify(x,False))
-#     return d
-
-# def parse_tags(fileName):
-#     root = ET.parse(fileName).getroot()
-#     return listify(root)
 
 def parse(fileName):
     root = ET.parse(fileName).getroot()
@@ -37,5 +28,4 @@ def parse(fileName):
 if __name__=="__main__":
     
     print(parse('thefile.html'))
-    # print(parse_tags('thefile.html'))
 
